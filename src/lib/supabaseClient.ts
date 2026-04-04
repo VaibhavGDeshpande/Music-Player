89022ce7cb80a0795f91b6e47f9234a8ddf9e6ff
VaibhavGDeshpande <deshpande.vaibhav1012@gmail.com>
2026-04-04 12:18:33 +0530

    feat: Update supabase client initialization to remove fallback URLs for environment variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://uieqausjstnncpeqrpph.supabase.co",
-  process.env.SUPABASE_SERVICE_ROLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZXFhdXNqc3RubmNwZXFycHBoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDk2NDU2NSwiZXhwIjoyMDg2NTQwNTY1fQ.E1nzjgArQKZud_CuLII0LgCiHFvfYYsP-hgCThXJFQ4"
+  process.env.NEXT_PUBLIC_SUPABASE_URL !,
+  process.env.SUPABASE_SERVICE_ROLE_KEY !
 );
